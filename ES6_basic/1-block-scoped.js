303c9827ca1a8022c7df38f65669f011da09ff42
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-17 18:38:05 +0200

    try

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -5,9 +5,10 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     task = true;
     task2 = false;
-  } else {
-    task = false;
-    task2 = true;
   }
+
+  task = false;
+  task2 = true;
+
   return [task, task2];
 }
