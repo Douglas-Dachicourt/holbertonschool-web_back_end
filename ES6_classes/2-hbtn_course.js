a9d330ca9e1f57781bc970b098d5a27a9b231827
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-18 13:45:39 +0200

    try

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this_length;
+  }
+
+  get students() {
+    return this_students;
+  }
+
+  set name(newName) {
+    if (typeof newName === "string") {
+        this._name = newName;
+    }
+    throw new TypeError("Name must be a string");
+  }
+
+  set length(newLength) {
+    if (typeof newLength === "number") {
+        this._length = newLength;
+    }
+    throw new TypeError("Length must be a number");
+  }
+
+  set students(newStudents) {
+    if (typeof newStudents === "string") {
+        this._studens = newStudents;
+    }
+  }
+}
