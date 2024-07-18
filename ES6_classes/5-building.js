cafc17af551b07d075a91dcb95cd06672fae70ea
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-18 15:57:39 +0200

    try

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
     if (this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
+    this._sqft = sqft;
   }
 
   get sqft() {
