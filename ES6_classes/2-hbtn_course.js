76d7582276242469798a2cfc1cb8ff004f3a7f81
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-18 14:30:18 +0200

    try

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,22 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Number must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+    for (const student in students) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Each student must be a string');
+      }
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -19,19 +36,27 @@ export default class HolbertonCourse {
 
   set name(newName) {
     if (typeof newName !== 'string') {
-      throw new TypeError("Name must be a string");
+      throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
 
   set length(newLength) {
     if (typeof newLength !== 'number') {
-      throw new TypeError("Length must be a number");
+      throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
 
   set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    for (const student in newStudents) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Each student must be a string');
+      }
+    }
     this._students = newStudents;
   }
 }
