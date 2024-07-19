5c696792e475a0bf8e9354b239f7fb8609be367a
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-19 15:55:23 +0200

    try

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -17,8 +17,7 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton extends HolbertonClass {
-  constructor(firstName, lastName, holbertonClass, year, location) {
-    super(year, location);
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
@@ -28,12 +27,16 @@ export class StudentHolberton extends HolbertonClass {
     return `${this._firstName} ${this._lastName}`;
   }
 
-  get holbertonClass() {
-    return this._year && this._location;
+  get year() {
+    return this._holbertonClass.year;
+  }
+
+  get location() {
+    return this._holbertonClass.location;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this.fullName} - ${this.year} - ${this.location}`;
   }
 }
 
