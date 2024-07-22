492361dd4567de9ea22a3aed021a290cf2e9a6d4
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-22 17:45:36 +0200

    update

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,5 @@
 export default function hasValuesFromArray(set, array) {
-  const newSet = set;
-
-  const verification = array.every((elem) => newSet.has(elem));
+  const verification = array.every((elem) => set.has(elem));
 
   return verification;
 }
