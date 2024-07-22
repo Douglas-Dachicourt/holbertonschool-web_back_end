a31b0f57460ae59aa50308909d139686e6cb95f7
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-22 11:20:28 +0200

    add new js folder

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents() {
+  const arr = [];
+  const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
+  const studentFive = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  arr.push(studentOne, studentTwo, studentFive);
+
+  return arr;
+}
