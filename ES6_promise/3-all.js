da7e5e54d7607b652c6bf197c1e76d1dce19619b
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-23 16:43:56 +0200

    try

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch((error) => console.log('Signup system offline', error));
+    .catch((error) => {
+      console.log('Signup system offline', error);
+    });
 }
