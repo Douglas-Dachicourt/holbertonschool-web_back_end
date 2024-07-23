f8f67ad75e8bef81a06493047b3e4592e1eb78a0
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-23 15:11:39 +0200

    try

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+export default function handleResponseFromAPI(promise) {
+  let newPromise = promise;
+  newPromise = new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error({ Error }));
+    }
+  })
+    .then(console.log('Got a response from the API'));
+
+  return newPromise;
+}
