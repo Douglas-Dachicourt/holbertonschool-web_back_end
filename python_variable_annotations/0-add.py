6f1df8fa9bb6eae53f7dfb022e33058f5d4e2656
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-12 11:46:15 +0200

    try

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""Provide a function 'add' that return a sum (float type)"""
+
+
 def add(a: float, b: float) -> float:
+    """
+        Arguments : two float numbers a and b
+
+        Returns : the sum of the two passed a and b (float type)
+
+    """
     return a + b
