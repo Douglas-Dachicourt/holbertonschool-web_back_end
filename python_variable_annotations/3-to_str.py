ff5c807e16909bfcfb14d19a27a372b6a93c6129
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-12 13:47:47 +0200

    add file 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function that return a representation of a float number into string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Argument : a float number
+
+        Returns: representation of the float number into string
+    """
+    return str(n)
