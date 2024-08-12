41620ba701286f6930c1f75d93263eb05eed64db
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-12 11:53:12 +0200

    add file 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Provide a function that concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Arguments: two strings str1 and str2 - both string type
+
+        Returns: the concatenation of both str1 and str2 (string type)
+
+    """
+    return str1 + str2
