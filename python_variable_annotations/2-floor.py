02df9a1336fe8db24fdcaed4b64041c351122700
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-12 13:36:10 +0200

    add file 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+"""
+Function that return the floor of a float passed as argument.
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Argument : a float number
+
+        Returns: an int type number
+    """
+    return math.floor(n)
