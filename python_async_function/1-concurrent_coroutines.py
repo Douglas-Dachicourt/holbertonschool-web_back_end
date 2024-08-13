8fd17da6d0fb779ce296689e943b2f35bc97a4a7
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-13 14:35:36 +0200

    try

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    Function that spawn wait_random n times with the specified max_delay
+"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+"""
+    import of wait_random function previously coded
+"""
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+        Arguments:
+            - n: type int, represent the number of delays
+                inserted in our list
+            - max_delay: type int, represented by function
+            wait_random
+
+        Returns: a list of float number, respectively the delays
+        allowed by n times
+
+    """
+    array = []
+    for elem in range(0, n):
+        delays = await wait_random(max_delay)
+        array.append(delays)
+
+    array.sort()
+    return array
