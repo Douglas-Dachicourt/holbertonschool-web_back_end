4c367655abfcd375039aa437e96cf18248e98675
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-13 11:32:15 +0200

    try

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -21,9 +21,3 @@ async def wait_random(max_delay: int = 10):
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
-
-
-async def main():
-    await wait_random()
-
-asyncio.run(main())
