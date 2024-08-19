d7a6d824d4a2862734fd61c891517cce367df32e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-19 14:59:42 +0200

    add file 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Module that measures the runtime of executing async_comprehension
+    four times concurrently.
+"""
+import asyncio
+import time
+"""
+    import of asyncio and time modules
+"""
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+"""
+    import async_comprehension function from previous encoded file
+"""
+
+
+async def measure_runtime() -> float:
+    """
+        Couroutine function measure_runtime
+
+        Arguments: None
+
+        Returns: total time of the execution , return float number
+
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension())
+
+    end_time = time.perf_counter()
+
+    total_time = (end_time - start_time)
+
+    return (total_time)
