39888cc3156434b41dc6b24a4bf5b8f6c229fd93
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-19 11:24:44 +0200

    try

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -24,7 +24,6 @@ async def async_generator() -> AsyncGenerator[int, None]:
         float: A random float between 0 and 10.
 
     """
-    for _ in range(0, 10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        random_num = random.uniform(0, 10)
-        yield random_num
+        yield random.uniform(0, 10)
