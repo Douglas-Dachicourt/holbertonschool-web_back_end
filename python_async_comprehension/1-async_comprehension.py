5485b42128f071b3411b48dadcd672b2d5768045
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-19 14:00:47 +0200

    try

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,9 +4,10 @@
     comprehensing over async_generator, then return the 10 random numbers.
 
 """
+import asyncio
 from typing import List
 """
-    import List from typing module
+    import List from typing and asyncio modules
 """
 
 async_generator = __import__('-0-async_generator').async_generator
