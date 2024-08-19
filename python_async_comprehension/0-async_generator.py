f263faae546b75f994d0f99ea47a666a05e5f68f
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-19 14:18:24 +0200

    try

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    Module that display a coroutine called async_generator that takes
+    Module provides a coroutine called async_generator that takes
     no arguments.
 
     The coroutine will loop 10 times, each time asynchronously wait 1
@@ -20,6 +20,8 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
         Coroutine async_generator that yields a random float between
         0 and 10, ten times.
+
+        Arguments: None
 
         Yields:
         float: A random float between 0 and 10.
