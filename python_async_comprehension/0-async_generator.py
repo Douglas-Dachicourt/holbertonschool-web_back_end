2cb823c98f7362200bac458a99b4a6e4a82bcfd6
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-19 11:09:36 +0200

    add new folder

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Mdule that display a coroutine called async_generator that takes no arguments.
+
+The coroutine will loop 10 times, each time asynchronously wait 1
+second, then yield a random number between 0 and 10
+
+"""
+from typing import Generator
+import asyncio
+import random
+"""
+import of Generator from typing
+import of asyncio to use async/await
+import random to generate a random number
+"""
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """
+        Function async_generator that takes no argument
+
+        Return: Generator typing
+
+    """
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        random_num = random.uniform(0, 10)
+        yield random_num
