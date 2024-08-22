eac5c72c9b28eeb03734661de26e246ad74e7027
Douglas Dachicourt <8815@holbertonstudents.com>
2024-08-22 17:00:45 +0200

    add file 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client["logs"]
+    collection = db["nginx"]
+
+    num_logs = collection.count_documents({})
+    print("{} {}".format(num_logs, "logs"))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count_methods = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count_methods))
+
+    status_check_count = collection.count_documents({"method":
+                                                     "GET", "path":
+                                                     "/status"})
+    print("{} status check".format(status_check_count))
