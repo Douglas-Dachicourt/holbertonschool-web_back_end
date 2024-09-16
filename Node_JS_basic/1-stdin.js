66f3870461d9d47793c05cf395c3a15263f3f3a5
Douglas Dachicourt <8815@holbertonstudents.com>
2024-09-16 17:51:21 +0200

    try

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,9 +4,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   const yourNameIs = 'Your name is: ';
-  if (input !== null) {
-    process.stdout.write(`${yourNameIs}${input.trim()}\n`);
-  }
+  process.stdout.write(`${yourNameIs}${input.trim()}\n`);
+
 });
 
 process.stdin.on('end', () => {
