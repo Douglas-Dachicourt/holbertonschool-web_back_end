e6e5aa1e62b03d5592ed609e9379889e8b4848ea
Douglas Dachicourt <8815@holbertonstudents.com>
2024-09-16 14:10:00 +0200

    add file 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf-8');
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  const yourNameIs = 'Your name is: ';
+  if (input !== null) {
+    process.stdout.write(yourNameIs + input);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
