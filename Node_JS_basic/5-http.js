69e75cb00107175afa22b12851450f759d28e853
Douglas Dachicourt <8815@holbertonstudents.com>
2024-09-17 14:16:34 +0200

    try

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,19 @@
 const { argv } = require('process');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const http = require('http');
-
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-type': 'plain/text' });
-    const url = req.url;
+  res.writeHead(200, { 'Content-type': 'plain/text' });
+  const { url } = req;
 
-    if (url === '/') {
-        res.end('Hello Holberton School!');
-    } else if (url === '/students') {
-        console.log('This is the list of our students')
-        countStudents(argv[2])
-        .then(data => res.end(data));
-    }
-})
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    console.log('This is the list of our students');
+    countStudents(argv[2])
+      .then((data) => res.end(data));
+  }
+});
 
 app.listen(1245);
 
